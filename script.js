deb8aa390bd8a6c601d8e8d68490e73636e69779
андрей шпаков <shpandr@yandex.ru>
2024-01-28 14:45:37 +0700

    первая версия

--- script.js
+++ script.js
@@ -1,36 +1,81 @@
+const songs = {
+  0 : 'Иные времена.mp3',
+  1 : 'Частушки - пофигушки.mp3',    
+  2 : 'Рассказ брачного агента, бывшего евнуха.mp3',
+  3 : 'Свободная частица.mp3',
+  4 : 'Любовное чтиво.mp3',
+  5 : 'Астрологическая песня.mp3',
+  6 : 'Деревенька.mp3',
+  7 : 'Случай в Кремле.mp3',
+  8 : 'Суррогаты.mp3',
+  9 : 'Включайте поворотники.mp3',
+  10 : 'Разговор с поэтом.mp3',
+  11 : 'Марш гедонистов.mp3',
+  12 : 'Разговор с критиком.mp3',
+  13 : 'Инь и Ян.mp3',
+  14 : 'Былина о попсе.mp3',
+  15 : 'Кто стучится в дверь ко мне.mp3',
+  16 : 'Товарищи учёные - 30 лет спустя.mp3',
+  17 : 'Хоронила мафия....mp3',
+  18 : 'Аутотренинг.mp3',
+  19 : 'Цыганская песня.mp3',
+  20 : 'О судьбе интеллигенции.mp3',
+  21 : 'Сказки нашего времени.mp3',
+  22 : 'Волшебство виски.mp3'
+} 
 
-function getSource(name){
+function getSource(id){  
 
-  const songs = {
+  return `music/${songs[id]}`
 
-    1 : 'Частушки - пофигушки.mp3',    
-    2 : 'Рассказ брачного агента, бывшего евнуха.mp3',
-    3 : 'Свободная частица.mp3',
-
-    
-  } 
-  
-  return songs[name]
-    
 }
+
+  function setColors() {
+
+    for (const key in songs){      
+
+      document.getElementById(key).style.color = 'var(--button-color)'
+
+    }
+
+  }   
+
+  window.addEventListener("DOMContentLoaded", ()=>{
+
+    createButtons(Object.keys(songs).length)   
+
+  })
+
 
 document.addEventListener('click', function(event){
 
   const className = event.target.className
 
-  const songName = event.target.name
-
   if(className == 'song'){
 
-    let filePath  = getSource(songName)
+    setColors()
 
-    document.getElementById('mp3_source').src = filePath
+    const songId = event.target.id
 
-    //audio.autoplay = true;
+    let filePath  = getSource(songId)
 
+    document.getElementById('mp3_source').src = filePath 
+
+    document.getElementById(songId).style.color = 'var(--button-pressed-color)'
+    
   }
 
 })
+
+function createButtons(count){
+  for(let i = 0; i < count; i++){
+    let btn = document.createElement('button')
+    btn.innerText=songs[i].replace('.mp3', '')
+    btn.className = 'song'
+    btn.id = i
+    document.querySelector('#button-container').appendChild(btn)
+  }
+}
 
 
 
